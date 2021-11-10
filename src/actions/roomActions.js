e504a4536d756edc757f335e5f268b42049ef1db
Raven <ravenkoehler@gmail.com>
2021-11-10 11:27:01 -0500

    added postRoom function

--- src/actions/roomActions.js
+++ src/actions/roomActions.js
@@ -10,3 +10,19 @@ export const fetchRooms = () => {
     }
 
 }
+
+export const postRoom = (data) => {
+    return (dispatch) => {
+        dispatch({type: "LOADING_ROOMS"})
+        fetch('https://localhost3000/rooms', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(data)
+          }
+        )
+    }
+
+}
