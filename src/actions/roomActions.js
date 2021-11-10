673a8709ebb0effd1695b7b6a51040f84b8e926c
Raven <ravenkoehler@gmail.com>
2021-11-10 08:51:06 -0500

    added fetchRooms action

---
+++ src/actions/roomActions.js
@@ -0,0 +1,11 @@
+export const fetchRooms = () => {
+    return (dispatch) => {
+        dispatch({type: "LOADING_ROOMS"})
+        fetch('https://localhost3000/rooms')
+        .then(response => response.json())
+        .then(responseJSON => {
+            dispatch({type: "ADD_ROOMS", rooms: responseJSON})
+        })
+    }
+
+}
--- src/reducers/roomsReducer.js
+++ src/reducers/roomsReducer.js
@@ -2,8 +2,24 @@ import uuid from 'react-uuid';
 
 export default function roomsReducer(state = { rooms: [], loading: false} , action) {
     switch ( action.type ) {
+        case "LOADING_ROOMS":
+            return {
+                ...state,
+                rooms:  [...state.rooms],
+                loading: true
+            };
+        case "ADD_ROOMS": 
+            return {
+                ...state,
+                rooms: state.rooms.concat(),
+                loading: false
+            }
         case "ADD_ROOM":
-            return {...state, rooms: state.rooms.concat({title: action.room, id: uuid()})}
+            return {
+                ...state, 
+                rooms: state.rooms.concat({title: action.room, id: uuid()}), 
+                loading: false
+            };
         default:
             return state
     } 
