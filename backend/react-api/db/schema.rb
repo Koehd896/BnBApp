6e53a130c2a340ff28cfbe70638971f105892631
Raven <ravenkoehler@gmail.com>
2021-11-19 11:59:16 -0500

    created user model

--- backend/react-api/db/schema.rb
+++ backend/react-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_173436) do
+ActiveRecord::Schema.define(version: 2021_11_19_165615) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 2021_11_11_173436) do
     t.string "image"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.integer "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_users_on_room_id"
+  end
+
+  add_foreign_key "users", "rooms"
 end
