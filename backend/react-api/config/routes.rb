1ea259ffe11213976b83e4b90aa58eda6d233e21
Raven <ravenkoehler@gmail.com>
2021-12-10 09:42:17 -0500

    changed routes to reflect new namespacing

--- backend/react-api/config/routes.rb
+++ backend/react-api/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users, only: [:create]
-  resources :bookings, only: [:create]
-  post '/login', to: 'auth#create'
+  namespace :api do
+    namespace :v1 do
+      resources :rooms
+      resources :users, only: [:create]
+      resources :bookings, only: [:create]
+    end
+  end
+  post '/api/v1/login', to: 'api/v1/auth#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
